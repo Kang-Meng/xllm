e94877d5c325524c8c35f50525b927b00081659c
liangzhiwei20 <liangzhiwei20@jd.com>
2025-08-14 14:10:44 +0800

    feat: support copy out blocks asynchronous.

--- xllm/core/runtime/worker_impl.h
+++ xllm/core/runtime/worker_impl.h
@@ -149,6 +149,8 @@ class WorkerImpl {
 
   Status get_status() const { return status_; }
 
+  folly::SemiFuture<bool> copy_out_blocks_async(InputParameters& input_params);
+
  private:
   void update_last_step_output(const std::optional<ForwardOutput>& output);
 
@@ -165,6 +167,10 @@ class WorkerImpl {
   // if enable_schedule_overlap, two step tasks might be dispatched to
   // the task queue, step need to be executed one-by-one
   ThreadPool threadpool_;
+
+  // extra working thread
+  // do some overlap work with model execute
+  ThreadPool extra_threadpool_;
 
   // dtype of the model
   torch::ScalarType dtype_;
@@ -204,6 +210,7 @@ class WorkerImpl {
   // c10_npu::NPUStream related files.
   struct NPUStreamHelper;
   std::unique_ptr<NPUStreamHelper> npu_stream_helper_;
+  std::unique_ptr<NPUStreamHelper> extra_stream_helper_;
 
   bool is_spec_draft_ = false;
 
